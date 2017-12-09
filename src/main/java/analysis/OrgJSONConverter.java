bded175b94e9970e4909894cac2696e18ae086aa
yamorist <leiaorgana228@gmail.com>
2017-12-10 00:37:29 +0200

    Added containers for data that will be inserted to database

--- src/main/java/analysis/OrgJSONConverter.java
+++ src/main/java/analysis/OrgJSONConverter.java
@@ -8,17 +8,13 @@ import org.apache.http.impl.client.CloseableHttpClient;
 
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
-import org.json.HTTP;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.Objects;
+import java.util.*;
 
 /**
  * This class provides methods for parsing objects from JSON format to RepositoryInfo.
@@ -27,31 +23,84 @@ public class OrgJSONConverter {
 
     /**
      * Token is set second time because we have to make another connection while creating the list of repository contributors.
-     * @see OrgJSONConverter#getAllContributors(String)
+     * @see OrgJSONConverter#getContributors(String)
      */
     private static final String TOKEN = System.getenv("GIT_TOKEN");
+    private Language language = new Language();
+    //for writing to db
+    private LinkedHashSet<String> languagesSet = new LinkedHashSet<>();
+    private LinkedHashSet<User> usersSet = new LinkedHashSet<>();
+    private LinkedHashSet<RepositoryInfo> globalReposSet = new LinkedHashSet<>();
+    private LinkedHashSet<ContributorInfo> contributorsSet = new LinkedHashSet<>();
+    private LinkedHashSet<RepositoryOwner> ownersSet = new LinkedHashSet<>();
+    private int langID = 1;
+
+    public TreeMap<Integer, String> getAllLanguages() {
+        return language.getLanguageMap();
+    }
+
+    public LinkedHashSet<User> getAllUsers() {
+        return usersSet;
+    }
+
+    public LinkedHashSet<RepositoryInfo> getAllRepositories() {
+        return globalReposSet;
+    }
+
+    public LinkedHashSet<ContributorInfo> getAllContributors() {
+        return contributorsSet;
+    }
+
+    public LinkedHashSet<RepositoryOwner> getAllOwners() {
+        return ownersSet;
+    }
 
     public ArrayList<RepositoryInfo> computeMostStarredRepos(String jsonString) throws URISyntaxException, IOException {
         if (jsonString.length() == 0) return null;
         JSONObject jsonObject = new JSONObject(jsonString);
         RepositoryInfo repository;
+        User user;
+        RepositoryOwner repOwner;
         ArrayList<RepositoryInfo> listOfRepos = new ArrayList<>();
-        JSONArray items = jsonObject.getJSONArray("items");
-        int repCounter = 0;
-        for (int i = 0; i < items.length(); i++) {
-            repository = new RepositoryInfo();
-            repository.setName(items.getJSONObject(i).getString("full_name"));
-            repository.setUrl(items.getJSONObject(i).getString("html_url"));
-            repository.setDescription((items.getJSONObject(i).isNull("description")) ? "No description found." : items.getJSONObject(i).getString("description"));
-            repository.setLanguage((items.getJSONObject(i).isNull("language")) ? "Language is not specified." : items.getJSONObject(i).getString("language"));
-            repository.setAmountOfStars(items.getJSONObject(i).getInt("stargazers_count"));
-            repository.setContributors(getTopContributors(getAllContributors(items.getJSONObject(i).getString("contributors_url"))));
-            repository.setTotalCommits(getTotalNumberOfCommits(getAllContributors(items.getJSONObject(i).getString("contributors_url"))));
-            listOfRepos.add(repository);
-            repCounter++;
-            System.out.printf("%d item analyzed \n", repCounter);
-        }
 
+        if (jsonObject.has("items")) {
+            JSONArray items = jsonObject.getJSONArray("items");
+            int repCounter = 0;
+            for (int i = 0; i < items.length(); i++) {
+                repository = new RepositoryInfo();
+                repository.setId(items.getJSONObject(i).getLong("id"));
+                repository.setName(items.getJSONObject(i).getString("full_name"));
+                repository.setUrl(items.getJSONObject(i).getString("html_url"));
+                repository.setDescription((items.getJSONObject(i).isNull("description")) ? "No description found." : items.getJSONObject(i).getString("description"));
+                repository.setLanguage((items.getJSONObject(i).isNull("language")) ? "Not specified." : items.getJSONObject(i).getString("language"));
+                repository.setAmountOfStars(items.getJSONObject(i).getInt("stargazers_count"));
+                repository.setContributors(getTopContributors(getContributors(items.getJSONObject(i).getString("contributors_url"))));
+                repository.setTotalCommits(getTotalNumberOfCommits(getContributors(items.getJSONObject(i).getString("contributors_url"))));
+                listOfRepos.add(repository);
+                globalReposSet.add(repository);
+
+                user = new User();
+                user.setId(items.getJSONObject(i).getJSONObject("owner").getLong("id"));
+                user.setName(items.getJSONObject(i).getJSONObject("owner").getString("login"));
+                usersSet.add(user);
+
+                repOwner = new RepositoryOwner();
+                repOwner.setUserID(items.getJSONObject(i).getJSONObject("owner").getLong("id"));
+                repOwner.setRepositoryID(items.getJSONObject(i).getLong("id"));
+                ownersSet.add(repOwner);
+
+                languagesSet.add((items.getJSONObject(i).isNull("language")) ? "Not specified." : items.getJSONObject(i).getString("language"));
+                repCounter++;
+                System.out.printf("%d item analyzed \n", repCounter);
+            }
+
+            for (String lang : languagesSet) {
+                if (!(language.getLanguageMap().containsValue(lang))) {
+                    language.setLanguageMap(langID, lang);
+                    langID++;
+                }
+            }
+        }
         return listOfRepos;
     }
 
@@ -66,24 +115,53 @@ public class OrgJSONConverter {
         if (jsonString.length() == 0) return null;
         JSONObject jsonObject = new JSONObject(jsonString);
         RepositoryInfo repository;
+        User user;
+        RepositoryOwner repOwner;
         ArrayList<RepositoryInfo> listOfRepos = new ArrayList<>();
         ArrayList<RepositoryInfo> mostCommittedRepos = new ArrayList<>();
-        JSONArray items = jsonObject.getJSONArray("items");
-        for (int i = 0; i < items.length(); i++) {
-            repository = new RepositoryInfo();
-            repository.setName(items.getJSONObject(i).getString("full_name"));
-            repository.setUrl(items.getJSONObject(i).getString("html_url"));
-            repository.setDescription((items.getJSONObject(i).isNull("description")) ? "No description." : items.getJSONObject(i).getString("description"));
-            repository.setLanguage((items.getJSONObject(i).isNull("language")) ? "Language is not specified." : items.getJSONObject(i).getString("language"));
-            repository.setAmountOfStars(items.getJSONObject(i).getInt("stargazers_count"));
-            repository.setContributors(getTopContributors(getAllContributors(items.getJSONObject(i).getString("contributors_url"))));
-            repository.setTotalCommits(getTotalNumberOfCommits(getAllContributors(items.getJSONObject(i).getString("contributors_url"))));
-            listOfRepos.add(repository);
+        if (jsonObject.has("items")) {
+            JSONArray items = jsonObject.getJSONArray("items");
+            int repCounter = 0;
+            for (int i = 0; i < items.length(); i++) {
+                repository = new RepositoryInfo();
+                repository.setId(items.getJSONObject(i).getLong("id"));
+                repository.setName(items.getJSONObject(i).getString("full_name"));
+                repository.setUrl(items.getJSONObject(i).getString("html_url"));
+                repository.setDescription((items.getJSONObject(i).isNull("description")) ? "No description." : items.getJSONObject(i).getString("description"));
+                repository.setLanguage((items.getJSONObject(i).isNull("language")) ? "Language is not specified." : items.getJSONObject(i).getString("language"));
+                repository.setAmountOfStars(items.getJSONObject(i).getInt("stargazers_count"));
+                repository.setContributors(getTopContributors(getContributors(items.getJSONObject(i).getString("contributors_url"))));
+                repository.setTotalCommits(getTotalNumberOfCommits(getContributors(items.getJSONObject(i).getString("contributors_url"))));
+                listOfRepos.add(repository);
+                globalReposSet.add(repository);
+
+                user = new User();
+                user.setId(items.getJSONObject(i).getJSONObject("owner").getLong("id"));
+                user.setName(items.getJSONObject(i).getJSONObject("owner").getString("login"));
+                usersSet.add(user);
+
+                repOwner = new RepositoryOwner();
+                repOwner.setUserID(items.getJSONObject(i).getJSONObject("owner").getLong("id"));
+                repOwner.setRepositoryID(items.getJSONObject(i).getLong("id"));
+                ownersSet.add(repOwner);
+
+                languagesSet.add((items.getJSONObject(i).isNull("language")) ? "Not specified." : items.getJSONObject(i).getString("language"));
+
+                repCounter++;
+                System.out.printf("%d item analyzed \n", repCounter);
+            }
+
+            for (String lang: languagesSet) {
+                if (!(language.getLanguageMap().containsValue(lang))) {
+                    language.setLanguageMap(langID, lang);
+                    langID++;
+                }
+            }
+
+            listOfRepos.sort(Comparator.comparing(RepositoryInfo::getTotalCommits).reversed());
+
+            mostCommittedRepos.addAll(listOfRepos);
         }
-
-        listOfRepos.sort(Comparator.comparing(RepositoryInfo::getTotalCommits).reversed());
-
-        mostCommittedRepos.addAll(listOfRepos);
         return mostCommittedRepos;
     }
 
@@ -91,7 +169,7 @@ public class OrgJSONConverter {
      * @param listOfContributors Gets the list of all contributors in repository.
      * @return Returns number of commits made by contributors.
      */
-        public static int getTotalNumberOfCommits(ArrayList<ContributorInfo> listOfContributors) {
+        public int getTotalNumberOfCommits(ArrayList<ContributorInfo> listOfContributors) {
         int totalNumberOfCommits = 0;
         if (listOfContributors != null) {
             for (int i = 0; i < listOfContributors.size(); i++) {
@@ -105,7 +183,7 @@ public class OrgJSONConverter {
      * @param allContributors Gets the list of all contributors in repository.
      * @return Returns top 5 contributors in repository or all contributors if list size is less than 5.
      */
-        public static ArrayList<ContributorInfo> getTopContributors(ArrayList<ContributorInfo> allContributors) {
+        public ArrayList<ContributorInfo> getTopContributors(ArrayList<ContributorInfo> allContributors) {
             ArrayList<ContributorInfo> topContributors = new ArrayList<>();
             if (allContributors == null) return topContributors;
             if (allContributors.size() <= 5 ){
@@ -122,7 +200,7 @@ public class OrgJSONConverter {
      * @param contributorsURLString A string that represents uri string for creating query.
      * @return Returns a sorted in a descending order list by number of commits of all contributors in repository.
      */
-    public static ArrayList<ContributorInfo> getAllContributors(String contributorsURLString) {
+    public ArrayList<ContributorInfo> getContributors(String contributorsURLString) {
         ArrayList<ContributorInfo> listOfContributors = null;
         try {
             listOfContributors = new ArrayList<>();
@@ -140,21 +218,21 @@ public class OrgJSONConverter {
                     if (Objects.equals(responseString, "[]")) break;
                     JSONObject jsonObject = jsonArray.getJSONObject(i);
                     contributor = new ContributorInfo();
+                    contributor.setId(jsonObject.getLong("id"));
                     contributor.setName(jsonObject.getString("login"));
-                    contributor.setProfileURL(jsonObject.getString("html_url"));
                     contributor.setAmountOfCommits(jsonObject.getInt("contributions"));
                     listOfContributors.add(contributor);
+                    contributorsSet.add(contributor);
                 }
             } else return null;
         }
         catch (IOException e) {
-            e.getMessage();
+            e.printStackTrace();
         }
         catch (ParseException e) {
-            e.getMessage();
+            e.printStackTrace();
         }
         catch (JSONException e) {
-            e.getMessage();
             e.printStackTrace();
         }
 
@@ -162,4 +240,5 @@ public class OrgJSONConverter {
         listOfContributors.sort(Comparator.comparing(ContributorInfo::getAmountOfCommits).reversed());
         return listOfContributors;
     }
+
 }
